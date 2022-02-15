1b7fe882475be93359e9b81d39148321afd12ef7
Augusto-Pellizzari <apluna_op1@hotmail.com>
2022-02-15 18:01:16 -0300

    Portfolio Start

---
+++ backend/src/test/java/com/augustoreact/Portfolio2022/Portfolio2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustoreact.Portfolio2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Portfolio2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
